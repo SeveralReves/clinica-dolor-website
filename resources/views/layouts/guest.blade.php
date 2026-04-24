6e107478f77d459116818ee733bc20e2df5bc99a
Gabo Duran <gabrielalexander2@gmail.com>
2026-04-24 09:48:13 -0400

    feat: agregar favicon a la plantilla de invitado

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -11,6 +11,7 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
+        <link rel="icon" type="image/svg+xml" href="{{ asset('/images/logos/logo.svg') }}">
         <!-- Scripts -->
         @vite(['resources/css/app.scss', 'resources/js/app.js'])
     </head>
